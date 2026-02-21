8e858e2fdcb0cb5d7bf62aa777b0f469ea4ef9c4
areeb <areebulhaq2005@gmail.com>
2026-02-22 01:40:57 +0500

    Implement insertValueAfterSpecificValue and insertValueBeforeSpecificValue methods in ArrayList class

--- static_array_list/LT_02.cpp
+++ static_array_list/LT_02.cpp
@@ -71,6 +71,117 @@ class ArrayList{
              }
 
         }
+        void insertValueAfterSpecificValue(int value , int input){
+             if(size==0){
+               cout << "list is empty . Initializing Array with your input";
+               array[0]=input;
+               size++;
+            }
+            else if(size==total_capacity){
+                int* array2 = new int[total_capacity*2];
+                total_capacity*=2;
+                int indexOfValue = returnIndex(value);
+                if(indexOfValue== -1){
+                    cout<<"the value u entered is not found in the array.";
+                }else{
+                    
+                    for(int i = 0 ; i <= indexOfValue;i++){
+                    array2[i]=this->array[i];
+                    }
+                    array2[indexOfValue+1]=input;
+                    for(int i = indexOfValue+1 ; i < size ; i++){
+                        array2[i+1]=array[i];
+                    }
+                    array=array2;
+                    size++;
+                
+                }
+            }else{
+                int indexOfValue = returnIndex(value);
+                if(indexOfValue== -1){
+                    cout<<"the value u entered is not found in the array.";
+                }else{
+                    int* array2 = new int[total_capacity];
+                    for(int i = 0 ; i <= indexOfValue;i++){
+                    array2[i]=this->array[i];
+                    }
+                    array2[indexOfValue+1]=input;
+                    for(int i = indexOfValue+1 ; i < size ; i++){
+                        array2[i+1]=array[i];
+                    }
+                    array=array2;
+                    size++;
+                }
+
+
+
+             }
+        }
+
+         void insertValueBeforeSpecificValue(int value , int input){
+             if(size==0){
+               cout << "list is empty . Initializing Array with your input";
+               array[0]=input;
+               size++;
+            }
+            else if(size==total_capacity){
+                int* array2 = new int[total_capacity*2];
+                total_capacity*=2;
+                int indexOfValue = returnIndex(value);
+                if(indexOfValue== -1){
+                    cout<<"the value u entered is not found in the array.";
+                }else{
+                    int indexOfValue = returnIndex(value);
+                if(indexOfValue== -1){
+                    cout<<"the value u entered is not found in the array.";
+                }else{
+                   
+                    for(int i = 0 ; i < indexOfValue;i++){
+                    array2[i]=this->array[i];
+                    }
+                    array2[indexOfValue]=input;
+                     size++;
+                    for(int i = indexOfValue+1 ; i < size ; i++){
+                        array2[i]=array[i-1];
+                    }
+                    array=array2;
+                   
+                }
+                   
+                
+                }
+            }else{
+                int indexOfValue = returnIndex(value);
+                if(indexOfValue== -1){
+                    cout<<"the value u entered is not found in the array.";
+                }else{
+                    int* array2 = new int[total_capacity];
+                    for(int i = 0 ; i < indexOfValue;i++){
+                    array2[i]=this->array[i];
+                    }
+                    array2[indexOfValue]=input;
+                    size++;
+                    for(int i = indexOfValue+1 ; i < size ; i++){
+                        array2[i]=array[i-1];
+                    }
+                    array=array2;
+                    
+                }
+
+
+
+             }
+        }
+        int returnIndex(int value){
+
+                for (int i = 0 ; i < this->size ; i ++) {
+                    if(array[i]==value){
+                        return i;
+                    }
+                }
+                return -1 ;
+                
+        }
         void display(){
             cout << "{ ";
             for (int i = 0 ; i < this->size ; i++){
@@ -82,10 +193,26 @@ class ArrayList{
 int main(){
     ArrayList list = ArrayList();
     list.insertValueAtTheEndOfList(55);
-    list.insertValueAtTheEndOfList(99);
-    list.insertValueAtTheEndOfList(65);
-    list.insertValueAtTheEndOfList(22);
-    list.insertValueAtTheStartOfList(1);
+    list.insertValueAtTheEndOfList(2);
+    list.insertValueAtTheEndOfList(2);
+
+    list.insertValueAtTheEndOfList(435);
+    list.insertValueAtTheEndOfList(343);
+    list.insertValueAtTheEndOfList(5);
+    list.insertValueAtTheEndOfList(21);
+    list.insertValueAtTheEndOfList(254);
+    list.insertValueAtTheEndOfList(20);
+    list.insertValueAtTheEndOfList(244);
+    list.insertValueAtTheEndOfList(211);
+    list.insertValueAtTheEndOfList(2000);
+    list.insertValueAtTheEndOfList(2112);
+   
+   
+    list.insertValueBeforeSpecificValue(2112,1);
+    
+
+   
+
    
    
     list.display();
