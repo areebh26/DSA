8c82161349a597645c5fd0422ac47894ce3848e1
areeb <areebulhaq2005@gmail.com>
2026-02-22 20:50:51 +0500

    Add LinkedList implementation with insertAtEnd and display methods

--- singly_linked_list/SLL.cpp
+++ singly_linked_list/SLL.cpp
@@ -1,4 +1,9 @@
+#include <iostream>
+using namespace std;
+
+
 class Node{
+    public : 
     int data;
     Node* next;
     Node(int data){
@@ -7,10 +12,38 @@ class Node{
     }
 };
 class LinkedList{
+    public : 
     Node* head;
     Node* tail;
     LinkedList(){
         this->head=this->tail=nullptr;
     }
-    
+    void insertAtEnd(int num){
+        if(this->head==nullptr){
+            Node* newNode = new Node(num);
+            head=tail=newNode;
+        }else{
+             Node* newNode = new Node(num);
+             tail->next=newNode;
+             tail=newNode;
+        }
+    }
+    void display(){
+        Node* temp = head;
+        
+        while(temp!=nullptr){
+            cout << temp->data << "->";
+            temp=temp->next; 
+        }
+        cout << "NULL  ";
+    }
+
+};
+int main(){
+    LinkedList list = LinkedList();
+    list.insertAtEnd(1);
+    list.insertAtEnd(2);
+    list.insertAtEnd(3);
+    list.insertAtEnd(4);
+    list.display();
 }
